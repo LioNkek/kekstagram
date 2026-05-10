2cd592713086e4990d3fcf97539df78e80d13ac1
Lev <lion.gg.k@gmail.com>
2026-05-10 11:46:26 +0700

    asd

--- js/functions.js
+++ js/functions.js
@@ -1,19 +1,8 @@
-function checkStringLength(textToValidate, maxLength) {
-  return textToValidate.length <= maxLength;
-}
-
-const checkStringLength = (textToValidate, maxLength) => textToValidate.length <= maxLength;
-
-// Строка короче 20 символов
-checkStringLength('проверяемая строка', 20); // true
-// Длина строки ровно 18 символов
-checkStringLength('проверяемая строка', 18); // true
-// Строка длиннее 10 символов
-checkStringLength('проверяемая строка', 10); // false
-
-
 function isPalindrom(string) {
-  string = string.toLowerCase();
+  const cleaned = string.replaceAll(' ', '').toLowerCase();
+  const reversed = cleaned.split('').reverse().join('');
+
+  return cleaned === reversed;
 }
 
 // Строка является палиндромом
@@ -21,6 +10,20 @@ isPalindrom('топот'); // true
 // Несмотря на разный регистр, тоже палиндром
 isPalindrom('ДовОд'); // true
 // Это не палиндром
-isPalindrom('Кекс');  // false
+isPalindrom('Кекс'); // false
 // Это палиндром
 isPalindrom('Лёша на полке клопа нашёл '); // true
+
+
+function checkStringLength(textToValidate, maxLength) {
+  return textToValidate.length <= maxLength;
+}
+
+// const checkStringLength = (textToValidate, maxLength) => textToValidate.length <= maxLength;
+
+// Строка короче 20 символов
+checkStringLength('проверяемая строка', 20); // true
+// Длина строки ровно 18 символов
+checkStringLength('проверяемая строка', 18); // true
+// Строка длиннее 10 символов
+checkStringLength('проверяемая строка', 10); // false
